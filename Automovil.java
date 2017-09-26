c20bce5a1dbe42aa3fb2eb51dc355af9f507016c
Victor Orozco <vorozco@nabenik.com>
2017-09-26 14:06:55 -0600

    Agregando implementacion de auto en Java

---
+++ Automovil.java
@@ -0,0 +1,18 @@
+public class Automovil{
+	private String color;
+	private int modelo; //Fabricacion
+
+	public void setColor(String color){
+		this.color = color;
+	}
+	public void setModelo(int modelo){
+		this.modelo = modelo;
+	}
+	public String getColor(){
+		return color;
+	}
+
+	public int getModelo(){
+		return modelo;}
+
+}
